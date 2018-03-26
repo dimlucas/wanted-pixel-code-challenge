23bb087feab78082dd766a112206f4e547a298d4
Dimitris Loukas <dimlucas@outlook.com>
2018-03-26 23:33:43 +0300

    Add users service

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { User } from '../models/user';
+import { Post } from '../models/post';
+import { Comment } from '../models/comment';
+import 'rxjs/add/operator/toPromise';
+import { Comment } from '@angular/compiler';
+
+@Injectable()
+export class UsersService {
+    users: User[] = [];
+    posts: Post[] = [];
+    comments: Comment[] = [];
+    private _baseUrl: string = "http://jsonplaceholder.typicode.com/";
+
+    constructor(private _http: HttpClient) {
+
+    }   
+    
+    fetchUsers(): Promise<User[]> {
+        return this._http.get<User[]>(`${this._baseUrl}/users`).toPromise()
+    }    
+
+    fetchPosts(): Promise<Post[]> {
+        return this._http.get<Post[]>(`${this._baseUrl}/posts`).toPromise();
+    }
+
+    fetchComments(): Promise<Comment[]> {
+        return this._http.get<Comment[]>(`${this._baseUrl}/comments`).toPromise();
+    }
+}
