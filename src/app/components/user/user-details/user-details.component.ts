1bd69e69efb0ad027f0dfe25566de78e02f504dd
Dimitris Loukas <dimlucas@outlook.com>
2018-03-28 02:04:45 +0300

    User component

---
+++ src/app/components/user/user-details/user-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'user-details',
+    templateUrl: './user-details.component.html'
+})
+export class UserDetailsComponent {
+    
+}
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import { StateService } from "../../services/state.service";
+import { User } from "../../models/user";
 
 @Component({
     selector: 'user',
     templateUrl: './user.component.html'
 })
 export class UserComponent {
+    get user(): User {
+        return this.stateService.currentUser;
+    }
     
+    constructor(public stateService: StateService) {
+
+    }
 }
